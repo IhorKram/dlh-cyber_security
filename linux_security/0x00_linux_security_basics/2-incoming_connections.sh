4256e92376159ebe6d88f51661332933c711c519
IhorKram <kramarets1hor@gmail.com>
2026-05-04 16:23:38 +0200

    ¨linux_security¨

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-sudo iptables -F
-sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -P INPUT DROP
-sudo ip6tables -F
-sudo ip6tables -P INPUT DROP
+echo -e "*filter\n:INPUT DROP\n-A INPUT -p tcp --dport 80 -j ACCEPT\nCOMMIT" | sudo iptables-restore
