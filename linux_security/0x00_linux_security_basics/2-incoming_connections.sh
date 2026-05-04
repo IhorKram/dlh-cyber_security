8c8774ca3a7d74991506981576f892dd355fae16
IhorKram <kramarets1hor@gmail.com>
2026-05-04 16:44:03 +0200

    ¨linux_security¨

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo iptables-restore < <(echo -e "*filter\n:INPUT DROP [0:0]\n:FORWARD DROP [0:0]\n:OUTPUT ACCEPT [0:0]\n-A INPUT -p tcp --dport 80 -j ACCEPT\n-A INPUT -s $1 -m state --state ESTABLISHED,RELATED -j ACCEPT\nCOMMIT")
+echo -e "default deny incoming\ndefault allow outgoing\nallow from $1 to any port 80 proto tcp\nenable" | xargs -I {} sudo ufw {}
