d9321cfd09ffa5398fbba00bd4c7695aabd8bdd0
IhorKram <kramarets1hor@gmail.com>
2026-05-04 16:38:11 +0200

    ¨linux_security¨

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo iptables-restore < <(echo -e "*filter\n:INPUT DROP [0:0]\n:FORWARD DROP [0:0]\n:OUTPUT ACCEPT [0:0]\n-A INPUT -p tcp --dport 80 -j ACCEPT\n-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT\nCOMMIT")
+sudo iptables-restore < <(echo -e "*filter\n:INPUT DROP [0:0]\n:FORWARD DROP [0:0]\n:OUTPUT ACCEPT [0:0]\n-A INPUT -p tcp --dport 80 -j ACCEPT\n-A INPUT -s $1 -m state --state ESTABLISHED,RELATED -j ACCEPT\nCOMMIT")
