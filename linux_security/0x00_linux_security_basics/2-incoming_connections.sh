32c36d5d5119afeaf17da06bcbc94eee8e0b84d3
IhorKram <kramarets1hor@gmail.com>
2026-05-04 16:04:21 +0200

    ¨linux_security¨

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-iptables -F
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -P INPUT DROP
+sudo iptables -F
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -P INPUT DROP
+sudo ip6tables -F
+sudo ip6tables -P INPUT DROP
