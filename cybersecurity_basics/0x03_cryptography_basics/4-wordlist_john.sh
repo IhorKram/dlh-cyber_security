af702d4a3c66eb84b5088e06f260d6ec0178e4ef
IhorKram <kramarets1hor@gmail.com>
2026-05-19 12:26:30 +0200

    next task

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/john/password.lst "$1" && john --show "$1" | awk -F: 'NR>1 {print $2}' > 4-password.txt
+john --wordlist=/usr/share/john/password.lst "$1" && john --show "$1" | awk -F: '{print $2}' > 4-password.txt
