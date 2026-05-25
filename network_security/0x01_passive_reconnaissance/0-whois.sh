64930908d41b12d389cdc696b7ec56063bb0baab
IhorKram <kramarets1hor@gmail.com>
2026-05-25 14:50:14 +0200

    next task

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': +' '/^(Registrant|Admin|Tech)/ {f=$1; v=$2; if(f ~ /Street$/) v=v" "; if(f ~ /Ext$/) f=f":"; a[++c]=f","(v!=""?" "v:"")} END {for(i=1;i<=c;i++) printf "%s", a[i] (i==c?"":"\n")}' > "$1.csv"
+whois "$1" | awk -F': +' '/^(Registrant|Admin|Tech)/ {f=$1; v=$2; if(f ~ /Street$/) v=v" "; if(f ~ /Ext$/) f=f":"; a[++c]=f", "v" "} END {for(i=1;i<=c;i++) printf "%s", a[i] (i==c?"":"\n")}' > "$1.csv"
