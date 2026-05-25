332da05aeacfc97ce620d0f29dbb9457c6e77543
IhorKram <kramarets1hor@gmail.com>
2026-05-25 11:58:57 +0200

    next task

--- linux_security/network_protocols/6-nfs.sh
+++ linux_security/network_protocols/6-nfs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for ip in $(nmap -p 111 --open -oG - 192.168.1.0/24 | awk '/Up$/ {print $2}'); do showmount -e $ip; done
+for ip in $(nmap -p 111 --open -oG - $1 | awk '/Up$/ {print $2}'); do showmount -e $ip; done
