e341389acfd12165e920b031599b6af97040e23f
IhorKram <kramarets1hor@gmail.com>
2026-05-25 11:56:45 +0200

    next task

--- linux_security/network_protocols/6-nfs.sh
+++ linux_security/network_protocols/6-nfs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for ip in $(nmap -p 111 --open -oG - 192.168.1.0/24 | awk '/Up$/ {print $2}'); do echo "=== IP: $ip ==="; showmount -e $ip; done
+for ip in $(nmap -p 111 --open -oG - 192.168.1.0/24 | awk '/Up$/ {print $2}'); do echo "Accessible NFS shares on $ip:"; showmount -e $ip; done
