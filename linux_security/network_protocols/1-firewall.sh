f13a97cb5050dfcc77ad66968fc009d04b2d7baf
IhorKram <kramarets1hor@gmail.com>
2026-05-25 11:31:30 +0200

    next task

--- linux_security/network_protocols/1-firewall.sh
+++ linux_security/network_protocols/1-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo iptables -P INPUT DROP; sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -A INPUT --dport ssh -j ACCEPT
+sudo iptables -A INPUT --dport ssh -p tcp -j ACCEPT
