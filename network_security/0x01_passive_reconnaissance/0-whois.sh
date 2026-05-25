4175394496deec345ee09fb6286c90ae4f984779
IhorKram <kramarets1hor@gmail.com>
2026-05-25 14:45:42 +0200

    next task

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': +' '
-/^(Registrant|Admin|Tech)/ {
-    f = $1; v = $2
-    if (f ~ /Street$/) v = v " "
-    if (f ~ /Ext$/) f = f ":"
-    
-    # Store lines in an array instead of printing immediately
-    lines[++count] = f "," v
-}
-END {
-    # Print lines with newlines between them, leaving no newline at the very end
-    for (i = 1; i <= count; i++) {
-        printf "%s", lines[i] (i == count ? "" : "\n")
-    }
-}' > "$1.csv"
+whois "$1" | awk -F': +' '/^(Registrant|Admin|Tech)/ {f=$1; v=$2; if(f ~ /Street$/) v=v" "; if(f ~ /Ext$/) f=f":"; a[++c]=f","v} END {for(i=1;i<=c;i++) printf "%s", a[i] (i==c?"":"\n")}' > "$1.csv"
