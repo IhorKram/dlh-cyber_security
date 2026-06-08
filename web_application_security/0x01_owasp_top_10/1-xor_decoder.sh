a27d5dd120a06bfbd5e2aedcd3c8d098cd60620e
IhorKram <kramarets1hor@gmail.com>
2026-06-08 10:29:44 +0200

    next task

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 hash="${1#\{xor\}}"
 
-echo "$hash" | base64 -d | xxd -p | tr -d '\n' | sed 's/../0x& /g' | while read -r -a bytes; do
-    for byte in "${bytes[@]}"; do
-        printf "\\x$(printf '%x' "$((byte ^ 0x5f))")"
-    done
-    echo ""
-done
+echo "$hash" | perl -e 'use MIME::Base64; print join("", map { chr(ord($_) ^ 0x5f); } split("", decode_base64(<>)))'
